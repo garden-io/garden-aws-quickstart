03aad32a7218a15fe876c6fe4974cf92b4ab4109
Anna Mager <anna@garden.io>
2023-03-23 17:30:51 +0100

    chore:use nested stack for ecr repos

--- cluster.ts
+++ cluster.ts
@@ -1,10 +1,11 @@
 import * as blueprints from '@aws-quickstart/eks-blueprints';
-import { GlobalResources, utils, ImportHostedZoneProvider} from '@aws-quickstart/eks-blueprints';
+import { GlobalResources, utils, ImportHostedZoneProvider } from '@aws-quickstart/eks-blueprints';
 import { Construct } from 'constructs';
 import { TeamPlatform } from './teams';
 import * as cdk from 'aws-cdk-lib'
-import * as ecr from 'aws-cdk-lib/aws-ecr'
-import { ECRRegistry } from './ecr';
+import * as eks from 'aws-cdk-lib/aws-eks';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { ECRRepository } from './ecr';
 
 //const burnhamManifestDir = './lib/teams/team-burnham/'
 //const rikerManifestDir = './lib/teams/team-riker/'
@@ -13,13 +14,18 @@ import { ECRRegistry } from './ecr';
 const accountID = process.env.CDK_DEFAULT_ACCOUNT!;
 const gitUrl = 'https://github.com/aws-samples/eks-blueprints-workloads.git';
 
-
 /**
  * See docs/patterns/nginx.md for mode details on the setup.
  */
-export default class DevCluster extends cdk.Stack{
-
+export default class DevCluster extends cdk.Stack {
+    // ecrRepos: ECRRepository
+    // constructor(scope: Construct, id: string) {
+    //     super(scope, id)
+    // }
     async buildAsync(scope: Construct, id: string) {
+        const repoNames: string[] = ["api", "result", "vote", "worker"]
+        new ECRRepository(repoNames, this, 'garden-repo')
+
         const teams: Array<blueprints.Team> = [
             new TeamPlatform(accountID)
         ];
@@ -27,13 +33,12 @@ export default class DevCluster extends cdk.Stack{
         const subdomain: string = utils.valueFromContext(scope, "dev.marketplace", "sys.garden");
 
         blueprints.HelmAddOn.validateHelmVersions = false;
-        
 
-        await blueprints.EksBlueprint.builder()
+        const cluster = await blueprints.EksBlueprint.builder()
             .account("049586690729")
             .region("eu-central-1")
             .teams(...teams)
-            .resourceProvider(GlobalResources.HostedZone ,new ImportHostedZoneProvider('Z028702323WOQ31QJAJJP', subdomain))
+            .resourceProvider(GlobalResources.HostedZone, new ImportHostedZoneProvider('Z028702323WOQ31QJAJJP', subdomain))
             .resourceProvider(GlobalResources.Certificate, new blueprints.CreateCertificateProvider('wildcard-cert', "*.dev.marketplace.sys.garden", GlobalResources.HostedZone))
             .addOns(
                 new blueprints.VpcCniAddOn(),
@@ -53,10 +58,9 @@ export default class DevCluster extends cdk.Stack{
                 }),
                 new blueprints.SecretsStoreAddOn({ rotationPollInterval: "120s" }),
                 new blueprints.ClusterAutoScalerAddOn)
-            .buildAsync(scope, `${id}-eks`);
-
-            blueprints.HelmAddOn.validateHelmVersions = false;
+            .buildAsync(scope, `${id}`)
+                
+        blueprints.HelmAddOn.validateHelmVersions = false;
+        cluster.getClusterInfo().nodeGroups?.at(0)?.role?.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonEC2ContainerRegistryPowerUser"));
     }
 }
-
-
--- ecr.ts
+++ ecr.ts
@@ -1,15 +1,44 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as s3 from 'aws-cdk-lib/aws-ecr';
 import { Construct } from 'constructs';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
 
-export class ECRRegistry extends cdk.Stack {
-    public readonly ecrRepo: ecr.Repository
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+
+// export class ECRRepository implements blueprints.ResourceProvider<ecr.IRepository> {
+//     provide(context: blueprints.ResourceContext): ecr.IRepository {
+//         return new ecr.Repository(context.scope, "vote-test");
+//     }
+export class ECRRepository extends cdk.NestedStack {
+    // public readonly ecrRepos: ecr.Repository[]
+    repoNames: string[]
+    constructor(repoNames: string[], scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
-        
-        this.ecrRepo = new ecr.Repository(this, 'backend', {
-            encryption: ecr.RepositoryEncryption.KMS,
-            imageScanOnPush: true,
-        });
+        this.repoNames = repoNames
+        // this.ecrRepos = []
+        // for (var repo of repoNames) {
+        //   this.ecrRepos.push(new ecr.Repository(this, repo, {
+        //       repositoryName: repo,
+        //       encryption: ecr.RepositoryEncryption.KMS,
+        //       imageScanOnPush: true,
+        //   }));
+        //   this.ecrRepos.push(new ecr.Repository(this, `${repo}/cache`, {
+        //       repositoryName: `${repo}/cache`,
+        //       encryption: ecr.RepositoryEncryption.KMS,
+        //       imageScanOnPush: true,
+        //   }));
+        // }
+        for (const repo of repoNames) {
+           new ecr.Repository(this, repo, {
+                repositoryName: repo,
+                encryption: ecr.RepositoryEncryption.KMS,
+                imageScanOnPush: true,
+            });
+            new ecr.Repository(this, `${repo}/cache`, {
+                repositoryName: `${repo}/cache`,
+                encryption: ecr.RepositoryEncryption.KMS,
+                imageScanOnPush: true,
+            });
+          }
     }
 }
