a2c3ea76a3ace1b824046c24df6e8be9cfc0363a
Anna Mager <78752267+twelvemo@users.noreply.github.com>
2023-04-03 16:20:28 +0200

    chore:use nested stack for ecr repos (#1)
    * chore:use nested stack for ecr repos
    
    * ecr nested stack works for single repo
    
    * chore: add ecrRepoNames input and loop over repos
    
    * chore: hardcode repos and create imagePullSecret
    
    * chore: add pullsecret without yaml manifest
    
    ---------
    
    Co-authored-by: Srihas Konduru <115004077+srihas-g@users.noreply.github.com>

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
-import * as cdk from 'aws-cdk-lib';
-import { logger } from '@aws-quickstart/eks-blueprints/dist/utils';
-import { HelmAddOn } from '@aws-quickstart/eks-blueprints';
+import * as cdk from "aws-cdk-lib";
+import { logger } from "@aws-quickstart/eks-blueprints/dist/utils";
+import { HelmAddOn } from "@aws-quickstart/eks-blueprints";
+import DevCluster from "./cluster";
 
 const app = new cdk.App();
 const account = "049586690729";
@@ -8,10 +9,6 @@ const region = "eu-central-1";
 const env: cdk.Environment = { account: account, region: region };
 HelmAddOn.validateHelmVersions = false;
 
-import DevCluster from './cluster';
-new DevCluster().buildAsync(app, 'dev-cluster').catch(() => {
-    logger.info("Error setting up dev cluster");
+new DevCluster().eksCluster(app, `dev-cluster`).catch(() => {
+  logger.info("Error setting up dev cluster");
 });
-
-import { ECRRegistry } from './ecr';
-new ECRRegistry(app, 'dev-cluster-ecr');
