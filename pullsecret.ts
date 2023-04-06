8759bf1b0d961b4cbcc5c668bc75ce19487a9447
Anna Mager <anna@garden.io>
2023-04-06 16:22:04 +0200

    feat: make cluster region and account agnostic and use context variables

--- pullsecret.ts
+++ pullsecret.ts
@@ -1,13 +1,18 @@
-import { KubernetesManifest } from "aws-cdk-lib/aws-eks";
 import { ClusterAddOn, ClusterInfo } from "@aws-quickstart/eks-blueprints";
-import * as blueprints from "@aws-quickstart/eks-blueprints";
 
+
+export interface ImagePullSecretAddOnProps{
+    readonly accountId: string
+    readonly region: string
+}
 export class DeployImagePullSecret implements ClusterAddOn {
+    readonly imagePullSecretAddOnProps: ImagePullSecretAddOnProps;
+    constructor(imagePullSecretAddOnProps: ImagePullSecretAddOnProps){
+        this.imagePullSecretAddOnProps = imagePullSecretAddOnProps
+    };
     deploy(clusterInfo: ClusterInfo): void {
-        const accountID = process.env.CDK_DEFAULT_ACCOUNT!;
         const cluster = clusterInfo.cluster;
-        const region = "eu-central-1"
-        const ecrURL = `${accountID}.dkr.ecr.${region}.amazonaws.com`
+        const ecrURL = `${this.imagePullSecretAddOnProps.accountId}.dkr.ecr.${this.imagePullSecretAddOnProps.region}.amazonaws.com`
         let secretValue = {
             credHelpers: {
                   [ecrURL]: "ecr-login"
--- teams.ts
+++ teams.ts
@@ -1,10 +1,30 @@
-import { PlatformTeam } from "@aws-quickstart/eks-blueprints";
+import { PlatformTeam, TeamProps } from "@aws-quickstart/eks-blueprints";
+import { ArnPrincipal } from "aws-cdk-lib/aws-iam";
 
+interface TeamPlatformProps {
+  userRoleArn?: string
+  users?: ArnPrincipal[]
+}
 export class TeamPlatform extends PlatformTeam {
-  constructor(accountID: string) {
-    super({
-      name: "platform",
-      userRoleArn: `arn:aws:iam::${accountID}:role/AWSReservedSSO_AdministratorAccess_b3c1cae6dc09120a`,
-    });
+  constructor(teamPlatformProps: TeamPlatformProps) {
+    if (teamPlatformProps.userRoleArn && teamPlatformProps.users){
+      super({
+        name: "platform",
+        userRoleArn: teamPlatformProps.userRoleArn,
+        users: teamPlatformProps.users
+      });
+    }
+    if (!teamPlatformProps.userRoleArn && teamPlatformProps.users){
+      super({
+        name: "platform",
+        users: teamPlatformProps.users
+      });
+    }
+    if (teamPlatformProps.userRoleArn && !teamPlatformProps.users){
+      super({
+        name: "platform",
+        userRoleArn: teamPlatformProps.userRoleArn
+      });
+    }
   }
 }
