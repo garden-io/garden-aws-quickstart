2671454818021b478ba46fb9eab2247ceaaa8ca1
Steffen Neubauer <steffen@garden.io>
2023-04-12 15:16:22 +0200

    chore: automated release pipeline (#4)
    Use release please
    
    Release-As: 0.1.0

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -4,17 +4,15 @@ Copyright (c) 2021 superwerker
 
 https://github.com/superwerker/superwerker/blob/078231a/cdk/cdk-bootstrap.py
 """
-import boto3
-from boto3.session import Session
 import subprocess
 import os
 
 class DeployError(Exception):
     pass
 
-cfn = boto3.client("cloudformation")
-s = Session()
-regions = [ 
+# cfn = boto3.client("cloudformation")
+# s = Session()
+regions = [
     "ap-northeast-1",
     "ap-northeast-2",
     "ap-south-1",
@@ -32,15 +30,27 @@ regions = [
     "us-west-2"
 ]
 
-template_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bootstrap.yaml")
-for region in regions:
+def cfn_apply(region, template, stack_name, extra_args=None):
+    template_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), template)
     cmd = ["aws", "cloudformation", "deploy",
-             "--stack-name", "garden-marketplace-assets-bootstrap",
+             "--stack-name", stack_name,
              "--region", region,
              "--template-file", template_file,
              ]
+    if extra_args:
+        cmd.extend(extra_args)
+
     print("Deploying to region={0} with cmd={1}".format(region, cmd))
     p = subprocess.run(cmd, capture_output=True)
     if p.returncode != 0:
         raise DeployError(p.stderr)
     print(p.stdout)
+
+
+# Release bucket, OIDC IAM role for release pipeline, ...
+cfn_apply("eu-central-1", "global-bootstrap.yaml", "garden-marketplace-global-bootstrap", extra_args=["--capabilities", "CAPABILITY_IAM"])
+
+# regional s3 buckets for CDK assets
+for region in regions:
+    cfn_apply(region, "regional-bootstrap.yaml", "garden-marketplace-assets-bootstrap")
+
