d75d126e9ebede338511ae302115eaaaea206200
Steffen Neubauer <stefreak@googlemail.com>
2023-04-12 14:05:50 +0200

    chore: boostrap, synthesize and parametrize (#3)
    * WIP synthesize and parametrize
    
    * chore: add bootstrap code and documentation
    
    * chore: more changes
    
    * chore: revert naming

--- cluster.ts
+++ cluster.ts
@@ -11,56 +11,24 @@ import { ECRRepository } from "./ecr";
 import { KubernetesVersion } from "aws-cdk-lib/aws-eks";
 import { DeployImagePullSecret } from "./pullsecret";
 import { ArnPrincipal } from "aws-cdk-lib/aws-iam";
-import { StackProps } from "aws-cdk-lib";
 
-const accountID = process.env.CDK_DEFAULT_ACCOUNT!;
-const gitUrl = "https://github.com/aws-samples/eks-blueprints-workloads.git";
-
+const PARAMETER_SUBDOMAIN = "subdomain"
+const PARAMETER_HOSTEDZONEID = "hostedZoneId"
 /**
  * See docs/patterns/nginx.md for mode details on the setup.
  */
-export default class DevCluster extends cdk.Stack {
-  async eksCluster(scope: Construct, id: string, props: StackProps) {
-    const accountId = cdk.Stack.of(this).account
-    const region = cdk.Stack.of(this).region
-
-
-    const subdomainName = new cdk.CfnParameter(this, "subdomain", {
-      type: "String",
-      description: "The subdomain that can be used for ingress to the development environments\
-       e.g. garden.mycompany.com. \
-       Needs to be a hosted domain in Route53RecordTarget."}).valueAsString;
-
-    //unfortunately we can't use this lookup for env agnostic stacks (need to specify region)
-    // https://docs.aws.amazon.com/cdk/v2/guide/resources.html#resources_external
-    // const domainID = route53.HostedZone.fromLookup(this, id, {domainName: subdomainName})
-
-    const hostedZoneId = new cdk.CfnParameter(this, "hostedZoneId", {
-      type: "AWS::Route53::HostedZone::Id",
-      description: "The ID of the Route53 hosted zone with the domain that can be used for ingress\
-       to the development environments"}).valueAsString;
-
-    const iamUsers = new cdk.CfnParameter(this, "iamUsers", {
-      type: "CommaDelimitedList",
-      default: "",
-      description: "Comma delimited list of IAM users principal ARNs that should get access to the dev cluster\
-       e.g. \"arn:aws:iam::123456789012:user/JohnDoe,arn:aws:iam::123456789012:user/Alice\""}).valueAsList;
-
-    const iamRole = new cdk.CfnParameter(this, "iamRole", {
-      type: "String",
-      default: "arn:aws:sts::049586690729:assumed-role/AWSReservedSSO_AdministratorAccess_b3c1cae6dc09120a",
-      description: "A role that should get access to the dev cluster e.g.\
-       \"arn:aws:iam::123456789023:role/AWSReservedSSO_PlatformEngineers_4ed12acae0543\""}).valueAsString;
-
-    // use context variables for now because we cannot use CfnParameters
-    // for these values since we are manipulating the values in the code
-    const iamUsersArnsCtx = this.node.tryGetContext('iamUsers').split(",").map((item: string) => new ArnPrincipal(item))
-    const iamRoleArnCtx = this.node.tryGetContext('iamRole')
-    const subdomainNameCtx = this.node.tryGetContext('subdomainName')
-    const hostedZoneIdCtx = this.node.tryGetContext('hostedZoneId')
+export class DevClusterConstruct {
+  async eksCluster(scope: Construct, id: string) {
+    // TODO
+    const iamRoleArnCtx = "arn:aws:iam::123456789023:role/AWSReservedSSO_PlatformEngineers_4ed12acae0543"
+    const iamUsersArnsCtx = [new ArnPrincipal(iamRoleArnCtx)]
     const teams: Array<blueprints.Team> = [new TeamPlatform({userRoleArn: iamRoleArnCtx, users: iamUsersArnsCtx})];
 
     blueprints.HelmAddOn.validateHelmVersions = false;
+
+    const accountId = cdk.Fn.sub("${AWS::AccountId}")
+    const region = cdk.Fn.sub("${AWS::Region}")
+
     const cluster = await blueprints.EksBlueprint.builder()
       .account(accountId)
       .region(region)
@@ -68,13 +36,13 @@ export default class DevCluster extends cdk.Stack {
       .teams(...teams)
       .resourceProvider(
         GlobalResources.HostedZone,
-        new ImportHostedZoneProvider(hostedZoneIdCtx, subdomainNameCtx)
+        new ImportHostedZoneProvider(cdk.Fn.ref(PARAMETER_HOSTEDZONEID), cdk.Fn.ref(PARAMETER_SUBDOMAIN))
       )
       .resourceProvider(
         GlobalResources.Certificate,
         new blueprints.CreateCertificateProvider(
           "wildcard-cert",
-          `*${subdomainNameCtx}`,
+          cdk.Fn.join(".", ["*", cdk.Fn.ref(PARAMETER_SUBDOMAIN)]),
           GlobalResources.HostedZone
         )
       )
@@ -90,19 +58,41 @@ export default class DevCluster extends cdk.Stack {
           version: "0.15.2",
           internetFacing: true,
           backendProtocol: "tcp",
-          externalDnsHostname: subdomainNameCtx,
+          externalDnsHostname: cdk.Fn.ref(PARAMETER_SUBDOMAIN),
           crossZoneEnabled: false,
           certificateResourceName: GlobalResources.Certificate,
         }),
         new blueprints.SecretsStoreAddOn({ rotationPollInterval: "120s" }),
         new blueprints.ClusterAutoScalerAddOn(),
-        new DeployImagePullSecret({accountId: accountId, region: region}),
+        new DeployImagePullSecret({ accountId, region }),
         new blueprints.NestedStackAddOn({
           builder: ECRRepository.builder(),
           id: "ecr-nested-stack"
         })
       )
-      .buildAsync(scope, `${id}`);
+      .buildAsync(scope, id);
+
+      // add parameters
+      // TODO: is there a better approach to add parameters to the EKS blueprint CFN stack?
+
+      new cdk.CfnParameter(cluster, PARAMETER_SUBDOMAIN, {
+        type: "String",
+        description: `
+          The subdomain that can be used for ingress to the development environments, e.g. garden.mycompany.com
+          Needs to be a hosted domain in Route53RecordTarget.
+        `
+      });
+
+    // unfortunately we can't use this lookup for env agnostic stacks (need to specify region)
+    // https://docs.aws.amazon.com/cdk/v2/guide/resources.html#resources_external
+    // const domainID = route53.HostedZone.fromLookup(this, id, {domainName: subdomainName})
+    new cdk.CfnParameter(cluster, PARAMETER_HOSTEDZONEID, {
+      type: "AWS::Route53::HostedZone::Id",
+      description: `
+        The ID of the Route53 hosted zone with the domain that can be used for ingress
+        to the development environments
+      `
+    });
 
     blueprints.HelmAddOn.validateHelmVersions = false;
     cluster
